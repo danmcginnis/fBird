481c58c8f13087c02ff3c772aee9677931e10ee0
Donald-Otto <donald.j.otto@gmail.com>
2014-04-25 01:26:46 -0400

    Code stripping mostly completed
    Bird graphic and background load. Game in paused state until spacebar is
    pressed, then switched to running. Checks to make sure bird does not fly
    to high.

--- src/kiloboltgame/Robot.java
+++ src/kiloboltgame/Robot.java
@@ -11,129 +11,50 @@ public class Robot {
 
 	private int centerX = 100;
 	private int centerY = 377;
-	private boolean jumped = false;
-	private boolean movingLeft = false;
-	private boolean movingRight = false;
-	private boolean ducked = false;
-	private boolean readyToFire = true;
 
-	private int speedX = 0;
 	private int speedY = 0;
 	public static Rectangle rect = new Rectangle(0, 0, 0, 0);
 	public static Rectangle rect2 = new Rectangle(0, 0, 0, 0);
 	public static Rectangle rect3 = new Rectangle(0, 0, 0, 0);
 	public static Rectangle rect4 = new Rectangle(0, 0, 0, 0);
-	public static Rectangle yellowRed = new Rectangle(0, 0, 0, 0);
-	public static Rectangle footleft = new Rectangle(0, 0, 0, 0);
-	public static Rectangle footright = new Rectangle(0, 0, 0, 0);
-
+	
 	private Background bg1 = StartingClass.getBg1();
 	private Background bg2 = StartingClass.getBg2();
-
-	private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 
 	public void update() {
 		// Moves Character or Scrolls Background accordingly.
 
-		if (speedX < 0) {
-			centerX += speedX;
-		}
-		if (speedX == 0 || speedX < 0) {
-			bg1.setSpeedX(0);
-			bg2.setSpeedX(0);
-
-		}
-		if (centerX <= 200 && speedX > 0) {
-			centerX += speedX;
-		}
-		if (speedX > 0 && centerX > 200) {
-			bg1.setSpeedX(-MOVESPEED / 5);
-			bg2.setSpeedX(-MOVESPEED / 5);
-		}
 
 		// Updates Y Position
 		centerY += speedY;
-
+		if (centerY<30)
+			centerY=30;
 		// Handles Jumping
+		if (speedY<=15)
 		speedY += 1;
+		
 
-		if (speedY > 3) {
-			jumped = true;
-		}
 
 		// Prevents going beyond X coordinate of 0
-		if (centerX + speedX <= 60) {
-			centerX = 61;
-		}
 
 		rect.setRect(centerX - 34, centerY - 63, 68, 63);
 		rect2.setRect(rect.getX(), rect.getY() + 63, 68, 64);
 		rect3.setRect(rect.getX() - 26, rect.getY() + 32, 26, 20);
 		rect4.setRect(rect.getX() + 68, rect.getY() + 32, 26, 20);
-		yellowRed.setRect(centerX - 110, centerY - 110, 180, 180);
-		footleft.setRect(centerX - 50, centerY + 20, 50, 15);
-		footright.setRect(centerX, centerY + 20, 50, 15);
 	}
 
-	public boolean isReadyToFire() {
-		return readyToFire;
-	}
 
-	public void setReadyToFire(boolean readyToFire) {
-		this.readyToFire = readyToFire;
-	}
-
-	public void moveRight() {
-		if (ducked == false) {
-			speedX = MOVESPEED;
-		}
-	}
-
-	public void moveLeft() {
-		if (ducked == false) {
-			speedX = -MOVESPEED;
-		}
-	}
-
-	public void stopRight() {
-		setMovingRight(false);
-		stop();
-	}
-
-	public void stopLeft() {
-		setMovingLeft(false);
-		stop();
-	}
 
 	private void stop() {
-		if (isMovingRight() == false && isMovingLeft() == false) {
-			speedX = 0;
-		}
-
-		if (isMovingRight() == false && isMovingLeft() == true) {
-			moveLeft();
-		}
-
-		if (isMovingRight() == true && isMovingLeft() == false) {
-			moveRight();
-		}
+		
 
 	}
 
 	public void jump() {
-		if (jumped == false) {
-			speedY = JUMPSPEED;
-			jumped = true;
-		}
-
+		speedY=-12;
+		
 	}
 
-	public void shoot() {
-		if (readyToFire) {
-			Projectile p = new Projectile(centerX + 50, centerY - 25);
-			projectiles.add(p);
-		}
-	}
 
 	public int getCenterX() {
 		return centerX;
@@ -143,13 +64,7 @@ public class Robot {
 		return centerY;
 	}
 
-	public boolean isJumped() {
-		return jumped;
-	}
 
-	public int getSpeedX() {
-		return speedX;
-	}
 
 	public int getSpeedY() {
 		return speedY;
@@ -163,44 +78,12 @@ public class Robot {
 		this.centerY = centerY;
 	}
 
-	public void setJumped(boolean jumped) {
-		this.jumped = jumped;
-	}
-
-	public void setSpeedX(int speedX) {
-		this.speedX = speedX;
-	}
 
 	public void setSpeedY(int speedY) {
 		this.speedY = speedY;
 	}
 
-	public boolean isDucked() {
-		return ducked;
-	}
 
-	public void setDucked(boolean ducked) {
-		this.ducked = ducked;
-	}
 
-	public boolean isMovingRight() {
-		return movingRight;
-	}
-
-	public void setMovingRight(boolean movingRight) {
-		this.movingRight = movingRight;
-	}
-
-	public boolean isMovingLeft() {
-		return movingLeft;
-	}
-
-	public void setMovingLeft(boolean movingLeft) {
-		this.movingLeft = movingLeft;
-	}
-
-	public ArrayList getProjectiles() {
-		return projectiles;
-	}
 
 }
--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -18,39 +18,32 @@ import kiloboltgame.framework.Animation;
 public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	enum GameState {
-		Running, Dead
+		Running, Dead, Paused
 	}
 
-	GameState state = GameState.Running;
+	GameState state = GameState.Paused;
 
-	private static Robot robot;
-	public static Heliboy hb, hb2;
+	static Robot robot;
 	public static int score = 0;
 	private Font font = new Font(null, Font.BOLD, 30);
 
-	private Image image, currentSprite, character, character2, character3,
-			characterDown, characterJumped, background, heliboy, heliboy2,
-			heliboy3, heliboy4, heliboy5;
+	private Image image, bird, background, upPipe, downPipe;
 
-	public static Image tilegrassTop, tilegrassBot, tilegrassLeft,
-			tilegrassRight, tiledirt;
 
 	private Graphics second;
 	private URL base;
 	private static Background bg1, bg2;
 	private Animation anim, hanim;
 
-	private ArrayList<Tile> tilearray = new ArrayList<Tile>();
-
 	@Override
 	public void init() {
 
-		setSize(800, 480);
+		setSize(480, 800);
 		setBackground(Color.BLACK);
 		setFocusable(true);
 		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
-		frame.setTitle("Q-Bot Alpha");
+		frame.setTitle("Flappy Bird");
 		try {
 			base = getDocumentBase();
 		} catch (Exception e) {
@@ -58,44 +51,13 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		}
 
 		// Image Setups
-		character = getImage(base, "data/character.png");
-		character2 = getImage(base, "data/character2.png");
-		character3 = getImage(base, "data/character3.png");
-
-		characterDown = getImage(base, "data/down.png");
-		characterJumped = getImage(base, "data/jumped.png");
-
-		heliboy = getImage(base, "data/heliboy.png");
-		heliboy2 = getImage(base, "data/heliboy2.png");
-		heliboy3 = getImage(base, "data/heliboy3.png");
-		heliboy4 = getImage(base, "data/heliboy4.png");
-		heliboy5 = getImage(base, "data/heliboy5.png");
-
-		background = getImage(base, "data/background.png");
-
-		tiledirt = getImage(base, "data/tiledirt.png");
-		tilegrassTop = getImage(base, "data/tilegrasstop.png");
-		tilegrassBot = getImage(base, "data/tilegrassbot.png");
-		tilegrassLeft = getImage(base, "data/tilegrassleft.png");
-		tilegrassRight = getImage(base, "data/tilegrassright.png");
-
+		bird = getImage(base, "data/flappybird.png");
+		bird=bird.getScaledInstance(51, 36, Image.SCALE_SMOOTH);
+		background = getImage(base, "data/background.jpg");
+		background = background.getScaledInstance(480,800,Image.SCALE_SMOOTH);
 		anim = new Animation();
-		anim.addFrame(character, 1250);
-		anim.addFrame(character2, 50);
-		anim.addFrame(character3, 50);
-		anim.addFrame(character2, 50);
+		anim.addFrame(bird, 1250);
 
-		hanim = new Animation();
-		hanim.addFrame(heliboy, 100);
-		hanim.addFrame(heliboy2, 100);
-		hanim.addFrame(heliboy3, 100);
-		hanim.addFrame(heliboy4, 100);
-		hanim.addFrame(heliboy5, 100);
-		hanim.addFrame(heliboy4, 100);
-		hanim.addFrame(heliboy3, 100);
-		hanim.addFrame(heliboy2, 100);
-
-		currentSprite = anim.getImage();
 	}
 
 	@Override
@@ -103,56 +65,9 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		bg1 = new Background(0, 0);
 		bg2 = new Background(2160, 0);
 		robot = new Robot();
-		// Initialize Tiles
-		try {
-			loadMap("data/map1.txt");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		hb = new Heliboy(340, 360);
-		hb2 = new Heliboy(700, 360);
 
 		Thread thread = new Thread(this);
 		thread.start();
-	}
-
-	private void loadMap(String filename) throws IOException {
-		ArrayList lines = new ArrayList();
-		int width = 0;
-		int height = 0;
-
-		BufferedReader reader = new BufferedReader(new FileReader(filename));
-		while (true) {
-			String line = reader.readLine();
-			// no more lines to read
-			if (line == null) {
-				reader.close();
-				break;
-			}
-
-			if (!line.startsWith("!")) {
-				lines.add(line);
-				width = Math.max(width, line.length());
-
-			}
-		}
-		height = lines.size();
-
-		for (int j = 0; j < 12; j++) {
-			String line = (String) lines.get(j);
-			for (int i = 0; i < width; i++) {
-
-				if (i < line.length()) {
-					char ch = line.charAt(i);
-					Tile t = new Tile(i, j, Character.getNumericValue(ch));
-					tilearray.add(t);
-				}
-
-			}
-		}
-
 	}
 
 	@Override
@@ -167,32 +82,13 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void run() {
-		if (state == GameState.Running) {
+		if (state == GameState.Running||state==GameState.Paused) {
 
 			while (true) {
-				robot.update();
-				if (robot.isJumped()) {
-					currentSprite = characterJumped;
-				} else if (robot.isJumped() == false
-						&& robot.isDucked() == false) {
-					currentSprite = anim.getImage();
-				}
-
-				ArrayList projectiles = robot.getProjectiles();
-				for (int i = 0; i < projectiles.size(); i++) {
-					Projectile p = (Projectile) projectiles.get(i);
-					if (p.isVisible() == true) {
-						p.update();
-					} else {
-						projectiles.remove(i);
-					}
-				}
-
-				updateTiles();
-				hb.update();
-				hb2.update();
+				
 				bg1.update();
-				bg2.update();
+				if (state == GameState.Running)
+					robot.update();
 				animate();
 				repaint();
 				try {
@@ -200,16 +96,13 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				if (robot.getCenterY() > 500) {
-					state = GameState.Dead;
-				}
+				
 			}
 		}
 	}
 
 	public void animate() {
 		anim.update(10);
-		hanim.update(50);
 	}
 
 	@Override
@@ -231,28 +124,18 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	@Override
 	public void paint(Graphics g) {
 
-		if (state == GameState.Running) {
+		if (state == GameState.Running||state==GameState.Paused) {
 
 			g.drawImage(background, bg1.getBgX(), bg1.getBgY(), this);
 			g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
-			paintTiles(g);
 
-			ArrayList projectiles = robot.getProjectiles();
-			for (int i = 0; i < projectiles.size(); i++) {
-				Projectile p = (Projectile) projectiles.get(i);
-				g.setColor(Color.YELLOW);
-				g.fillRect(p.getX(), p.getY(), 10, 5);
-			}
 
-			g.drawImage(currentSprite, robot.getCenterX() - 61,
+			g.drawImage(bird, robot.getCenterX() - 61,
 					robot.getCenterY() - 63, this);
-			g.drawImage(hanim.getImage(), hb.getCenterX() - 48,
-					hb.getCenterY() - 48, this);
-			g.drawImage(hanim.getImage(), hb2.getCenterX() - 48,
-					hb2.getCenterY() - 48, this);
+
 			g.setFont(font);
 			g.setColor(Color.WHITE);
-			g.drawString(Integer.toString(score), 740, 30);
+			g.drawString(Integer.toString(score), 400, 30);
 
 		} else if (state == GameState.Dead) {
 			g.setColor(Color.BLACK);
@@ -263,90 +146,40 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		}
 	}
 
-	private void updateTiles() {
-
-		for (int i = 0; i < tilearray.size(); i++) {
-			Tile t = (Tile) tilearray.get(i);
-			t.update();
-		}
-
-	}
-
-	private void paintTiles(Graphics g) {
-		for (int i = 0; i < tilearray.size(); i++) {
-			Tile t = (Tile) tilearray.get(i);
-			g.drawImage(t.getTileImage(), t.getTileX(), t.getTileY(), this);
-		}
-	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 
 		switch (e.getKeyCode()) {
-		case KeyEvent.VK_UP:
-			System.out.println("Move up");
-			break;
-
-		case KeyEvent.VK_DOWN:
-			currentSprite = characterDown;
-			if (robot.isJumped() == false) {
-				robot.setDucked(true);
-				robot.setSpeedX(0);
-			}
-			break;
-
-		case KeyEvent.VK_LEFT:
-			robot.moveLeft();
-			robot.setMovingLeft(true);
-			break;
-
-		case KeyEvent.VK_RIGHT:
-			robot.moveRight();
-			robot.setMovingRight(true);
-			break;
-
+			
 		case KeyEvent.VK_SPACE:
-			robot.jump();
-			break;
-
-		case KeyEvent.VK_CONTROL:
-			if (robot.isDucked() == false && robot.isJumped() == false
-					&& robot.isReadyToFire()) {
-				robot.shoot();
-				robot.setReadyToFire(false);
+			if (state == GameState.Running) {
+				robot.jump();
+				break;
+			}else if (state == GameState.Paused){
+				state=GameState.Running;
+				robot.jump();
+				break;
+			}else if (state == GameState.Dead){
+				state=GameState.Paused;
+				reset();
 			}
-			break;
+			
 
 		}
 
 	}
-
+	public void reset(){
+		
+	}
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
-		case KeyEvent.VK_UP:
-			System.out.println("Stop moving up");
-			break;
 
-		case KeyEvent.VK_DOWN:
-			currentSprite = anim.getImage();
-			robot.setDucked(false);
-			break;
-
-		case KeyEvent.VK_LEFT:
-			robot.stopLeft();
-			break;
-
-		case KeyEvent.VK_RIGHT:
-			robot.stopRight();
-			break;
 
 		case KeyEvent.VK_SPACE:
 			break;
 
-		case KeyEvent.VK_CONTROL:
-			robot.setReadyToFire(true);
-			break;
 
 		}
 
